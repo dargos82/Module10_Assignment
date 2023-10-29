9a235a272c41bdbbae6d5f8752141377315a479e
dargos82 <142857091+dargos82@users.noreply.github.com>
2023-10-29 18:01:55 -0400

    Test file input.

--- src/ReadCensus.java
+++ src/ReadCensus.java
@@ -1,9 +1,20 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class ReadCensus {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		File file = new File("/Users/davidblossomii/Desktop/EN605201/Module 10/"
+				+ "SmallAreaIncomePovertyEstData v2.txt");
+		Scanner scan = new Scanner(file);
+		
+		while(scan.hasNextLine())
+		{
+			System.out.println(scan.nextLine());
+		}
+		
+		
 
 	}
 
