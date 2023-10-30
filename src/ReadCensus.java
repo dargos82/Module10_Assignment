650ef203e8397ea1d078ee8c51f30023c7ba781e
dargos82 <142857091+dargos82@users.noreply.github.com>
2023-10-29 21:32:31 -0400

    Added "State" class.  Updated "ReadCensus".

--- src/ReadCensus.java
+++ src/ReadCensus.java
@@ -1,21 +1,35 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class ReadCensus {
+public class ReadCensus 
 
-	public static void main(String[] args) throws FileNotFoundException {
-		
-		File file = new File("/Users/davidblossomii/Desktop/EN605201/Module 10/"
+{
+
+   public static void main(String[] args) throws FileNotFoundException 
+   {
+      State s = new State();
+      
+      File file = new File("/Users/davidblossomii/Desktop/EN605201/Module 10/"
 				+ "SmallAreaIncomePovertyEstData v2.txt");
-		Scanner scan = new Scanner(file);
+      Scanner scan = new Scanner(file);
 		
-		while(scan.hasNextLine())
-		{
-			System.out.println(scan.nextLine());
-		}
+      while( scan.hasNextLine() )
+      {
+         int stateId = scan.nextInt();
+         
+         for(scan.nextInt() == stateId; )
+      }
 		
+      String newFileName = "Census_Report.txt";
+      File outputFile = new File( newFileName );
 		
-
-	}
+      if( outputFile.exists() );
+      {
+         System.out.println( "File already exists.  Program ending." );
+         System.exit(1);
+      }
+   }
+   
+   private int stateId;
 
 }
