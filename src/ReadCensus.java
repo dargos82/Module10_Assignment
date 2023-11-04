766672d4f857f14660c3c53d628919f659c5db77
dargos82 <142857091+dargos82@users.noreply.github.com>
2023-11-03 20:26:38 -0400

    File opens and reads.  Concept is to scan each line, store each of the relevant values in an ArrayList of that type, convert that ArrayList to an array, sum the values, then store the three values in an object that will then be called and printed to the new file.  Not sure if that'll work or if it's the correct concept but it's what I have for now.

--- src/ReadCensus.java
+++ src/ReadCensus.java
@@ -1,35 +1,73 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ReadCensus 
-
 {
-
-   public static void main(String[] args) throws FileNotFoundException 
+   //public ArrayList<Float> population = new ArrayList<Float>();
+   //public ArrayList<Float> childPopulation = new ArrayList<Float>();
+   //public ArrayList<Float> childPovertyPopulation = new ArrayList<Float>();
+   
+   public static void main(String[] args) throws IOException
    {
       State s = new State();
+      ArrayList<Float> population = new ArrayList<Float>();
+      ArrayList<Float> childPopulation = new ArrayList<Float>();
+      ArrayList<Float> childPovertyPopulation = new ArrayList<Float>();
       
-      File file = new File("/Users/davidblossomii/Desktop/EN605201/Module 10/"
-				+ "SmallAreaIncomePovertyEstData v2.txt");
-      Scanner scan = new Scanner(file);
-		
-      while( scan.hasNextLine() )
+      Scanner input = new Scanner(System.in);
+      
+      System.out.print( "Filename (include path and .txt): "); //input file
+      String fileName = input.nextLine();
+      System.out.print( "\nOutput filename: "); //output file
+      String outputFileName = input.nextLine();
+      System.out.print( "\nNumber of records: "); //# of records
+      int numberOfRecords = input.nextInt();
+      
+      File file = new File( fileName );  //create new File object
+      
+      BufferedReader fileInput = null;
+      String record = null;
+      
+      try
       {
-         int stateId = scan.nextInt();
+         fileInput = new BufferedReader( new FileReader( file ) );
          
-         for(scan.nextInt() == stateId; )
-      }
-		
-      String newFileName = "Census_Report.txt";
-      File outputFile = new File( newFileName );
-		
-      if( outputFile.exists() );
+         while( ( record = fileInput.readLine() ) !=  null)
+         {
+            String stateId = record.substring(1, 3);
+            //float districtId = Float.parseFloat( record.substring(4, 9) );
+            //String districtName = record.substring(10, 82);
+            float districtPop = Float.parseFloat( record.substring(83, 91));
+            population.add(districtPop); //add districtPop value to ArrayList
+            float districtChildPop = Float.parseFloat( record.substring(92, 100));
+            childPopulation.add(districtChildPop);
+            float districtChildPovertyPop = Float.parseFloat( record.substring(101, 109));
+            childPovertyPopulation.add(districtChildPovertyPop);
+            //String dataTag = record.substring(110, 131);
+         }
+         fileInput.close();
+         
+      } catch (FileNotFoundException e)
       {
-         System.out.println( "File already exists.  Program ending." );
-         System.exit(1);
+         e.printStackTrace();
       }
+      
+      
+      Integer sumPopulation[] = new Integer[population.size()]; //create new array
+      sumPopulation = population.toArray(sumPopulation); //convert ArrayList to array
+      
+      int totalPop = 0;
+      
+      for(int i : sumPopulation) //iterate through array
+      {
+         totalPop += i; //sum population values
+      }
+      
+      System.out.println( "total pop: " + totalPop );
+      
    }
    
-   private int stateId;
+   //private int stateId;
 
 }
