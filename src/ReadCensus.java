cfff901ad9de94e2e51201697db2bc94c92d1247
dargos82 <142857091+dargos82@users.noreply.github.com>
2023-11-05 08:19:04 -0500

    Read and output new file complete.

--- src/ReadCensus.java
+++ src/ReadCensus.java
@@ -1,31 +1,35 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class ReadCensus 
 {
-   //public ArrayList<Float> population = new ArrayList<Float>();
-   //public ArrayList<Float> childPopulation = new ArrayList<Float>();
-   //public ArrayList<Float> childPovertyPopulation = new ArrayList<Float>();
    
    public static void main(String[] args) throws IOException
-   {
-      State s = new State();
-      ArrayList<Integer> population = new ArrayList<Integer>();
-      ArrayList<Integer> childPopulation = new ArrayList<Integer>();
-      ArrayList<Integer> childPovertyPopulation = new ArrayList<Integer>();
+   {      
+      //State s = new State();
       
-      Scanner input = new Scanner(System.in);
+      //Variables for command-line parameters
+      String fileName = args[0];
+      String outputFileName = args[1];
+      int numberOfRecords = Integer.parseInt(args[2]);
       
-      System.out.print( "Filename (include path and .txt): "); //input file
-      String fileName = input.nextLine();
-      System.out.print( "\nOutput filename: "); //output file
-      String outputFileName = input.nextLine();
-      System.out.print( "\nNumber of records: "); //# of records
-      int numberOfRecords = input.nextInt();
+      File file = new File( fileName );  //create new File object for input file
+      File outputFile = new File( outputFileName ); //create new File object for output file
+      int[] population = new int[56];
+      //float[] population = new float[56];
+      int[] childPopulation = new int[56];
+      int[] childPovertyPopulation = new int[56];
+      //float calcPCP = 0;
+      //float[] percentChildPoverty = new float[56];
       
-      File file = new File( fileName );  //create new File object
+      //create output file with header info
+      PrintWriter fileOutput = new PrintWriter( new BufferedWriter( new FileWriter( outputFile, true) ) );
+
+      fileOutput.println("State     " + "Population  " + "Child Population  " + "Child Poverty Population  " +
+                       "% Child Poverty");
+      fileOutput.println("-----     " + "----------  " + "----------------  " + "------------------------  " +
+                       "---------------");
       
+      //initialize variables
       BufferedReader fileInput = null;
       String record = null;
       
@@ -35,50 +39,45 @@ public class ReadCensus
          
          while( ( record = fileInput.readLine() ) !=  null)
          {
-            //int stateId = Integer.parseInt(record.substring(1, 3));
+           
             String strStateId = record.substring(0, 3);
-            int stateId = Integer.parseInt(strStateId.trim() );
-            //float districtId = Float.parseFloat( record.substring(4, 9) );
-            //String districtName = record.substring(10, 82);
+            int stateId = Integer.parseInt( strStateId.trim() );
+            
             String strDistrictPop = record.substring(83, 91);
             int districtPop = Integer.parseInt( strDistrictPop.trim() );
-            population.add(districtPop); //add districtPop value to ArrayList
+            
             String strDistrictChildPop = record.substring(92, 100);
-            int districtChildPop = Integer.parseInt( strDistrictChildPop.trim());
-            childPopulation.add(districtChildPop);
+            int districtChildPop = Integer.parseInt( strDistrictChildPop.trim() );
+            
             String strDistrictChildPovertyPop = record.substring(101, 109);
-            int districtChildPovertyPop = Integer.parseInt( strDistrictChildPovertyPop.trim());
-            childPovertyPopulation.add(districtChildPovertyPop);
-            //String dataTag = record.substring(110, 131);
+            int districtChildPovertyPop = Integer.parseInt( strDistrictChildPovertyPop.trim() );
             
-            System.out.print( stateId );
-            System.out.print( "\t" + districtPop );
-            System.out.print( "\t" + districtChildPop );
-            System.out.println( "\t" + districtChildPovertyPop );
-            
+            population[stateId - 1] +=  districtPop;
+            childPopulation[stateId - 1] += districtChildPop;
+            childPovertyPopulation[stateId - 1] += districtChildPovertyPop;
+                
          }
-         fileInput.close();
-         
+
+         for(int i = 0; i <= 55; i++)
+         {
+            if(population[i] != 0)
+            {
+               float calcPCP = (100 * ((float) (childPovertyPopulation[i]) / childPopulation[i]));
+               int printStateId = i + 1;
+               
+               fileOutput.printf( "   %02d    %,11d  %,16d  %,24d  %15.2f %n", printStateId, population[i], 
+                     childPopulation[i], childPovertyPopulation[i], calcPCP);
+            }
+         }
+  
       } catch (FileNotFoundException e)
       {
          e.printStackTrace();
       }
       
-      
-      /*Integer sumPopulation[] = new Integer[population.size()]; //create new array
-      sumPopulation = population.toArray(sumPopulation); //convert ArrayList to array
-      
-      int totalPop = 0;
-      
-      for(int i : sumPopulation) //iterate through array
-      {
-         totalPop += i; //sum population values
-      }
-      
-      System.out.println( "total pop: " + totalPop );*/
-      
+      fileInput.close();
+      fileOutput.close();
+
    }
    
-   //private int stateId;
-
 }
