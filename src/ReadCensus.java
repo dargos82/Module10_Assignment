26e04219ff7d1c77b8d2362e3a3d5d8dce2a9b10
dargos82 <142857091+dargos82@users.noreply.github.com>
2023-12-10 14:16:11 -0500

    Updated

--- src/ReadCensus.java
+++ src/ReadCensus.java
@@ -1,25 +1,52 @@
 import java.io.*;
 
+/** class ReadCensus contains the objects, variables, and arrays needed to open, read, and output
+ * a new file. 
+ * 
+ * @authors George Haralampopulos and David Blossom
+ */
 public class ReadCensus 
 {
-   
+   /** main()
+    * This method creates the File objects, calls PrintWriter(), BufferedWriter(), FileWriter(),
+    * BufferedReader(), FileReader() and holds the variables and arrays to read, store, and write
+    * data.
+    * 
+    *  @param fileName
+    *  @param outputFileName
+    *  @param file
+    *  @param outputFile
+    *  @param population
+    *  @param childPopulation
+    *  @param childPovertyPopulation
+    *  @param fileOutput
+    *  @param fileInput
+    *  @param record
+    *  @param strStateId
+    *  @param stateId
+    *  @param strDistrictPop
+    *  @param districtPop
+    *  @param strDistrictChildPop
+    *  @param districtChildPop
+    *  @param strDistrictChildPovertyPop
+    *  @param districtChildPovertyPop
+    *  @param calcPCP
+    *  @param printStateId
+    */
    public static void main(String[] args) throws IOException
    {      
-      //State s = new State();
       
       //Variables for command-line parameters
-      String fileName = args[0];
-      String outputFileName = args[1];
-      int numberOfRecords = Integer.parseInt(args[2]);
+      String fileName = args[0]; //get String from command-line input
+      String outputFileName = args[1]; //get String from command-line input
       
       File file = new File( fileName );  //create new File object for input file
       File outputFile = new File( outputFileName ); //create new File object for output file
-      int[] population = new int[56];
-      //float[] population = new float[56];
+      
+      //create arrays to hold data from the input file
+      int[] population = new int[56]; 
       int[] childPopulation = new int[56];
       int[] childPovertyPopulation = new int[56];
-      //float calcPCP = 0;
-      //float[] percentChildPoverty = new float[56];
       
       //create output file with header info
       PrintWriter fileOutput = new PrintWriter( new BufferedWriter( new FileWriter( outputFile, true) ) );
@@ -34,7 +61,7 @@ public class ReadCensus
       String record = null;
       
       try
-      {
+      {     
          fileInput = new BufferedReader( new FileReader( file ) );
          
          while( ( record = fileInput.readLine() ) !=  null)
@@ -56,19 +83,22 @@ public class ReadCensus
             childPopulation[stateId - 1] += districtChildPop;
             childPovertyPopulation[stateId - 1] += districtChildPovertyPop;
                 
-         }
-
+         } //end while loop
+         
+         //iterate through arrays to get values and write to output file
          for(int i = 0; i <= 55; i++)
          {
-            if(population[i] != 0)
-            {
+            if(population[i] != 0) //logic not to write rows without any data; prevent divide by zero
+            {   
+               //calculate the percentage of children in poverty
                float calcPCP = (100 * ((float) (childPovertyPopulation[i]) / childPopulation[i]));
                int printStateId = i + 1;
                
+               //write selected data to output file
                fileOutput.printf( "   %02d    %,11d  %,16d  %,24d  %15.2f %n", printStateId, population[i], 
                      childPopulation[i], childPovertyPopulation[i], calcPCP);
-            }
-         }
+            } //end if statement
+         } //end for loop
   
       } catch (FileNotFoundException e)
       {
@@ -78,6 +108,6 @@ public class ReadCensus
       fileInput.close();
       fileOutput.close();
 
-   }
+   } //end main()
    
-}
+} //end class ReadCensus
--- src/WriteReport.java
+++
@@ -1,9 +0,0 @@
-
-public class WriteReport {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
